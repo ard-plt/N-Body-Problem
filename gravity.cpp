d01955d145568d1b3efc08e80a632869e4798730
Arda Polat <193826359+ard-plt@users.noreply.github.com>
2025-05-06 16:06:20 +0300

    gravity.cpp

--- gravity.cpp
+++ gravity.cpp
@@ -1,77 +1,163 @@
 #include <iostream>
 #include <cmath>
 
+const double G = 1.0;
+const double timestep = 0.2;
 
-const int G = 1;
-
-class vektor {
+class vector2D {
 public:
-    double x,y;
-    vektor(double x = 0, double y = 0) : x(x), y(y) {}
-    vektor topla(vektor v) const{ return vektor(x + v.x, y + v.y); }
-    vektor cikar(vektor v) const{ return vektor(x - v.x, y - v.y); }
-    vektor carp(double scalar) const{ return vektor(x * scalar, y * scalar); }
-    vektor bol(double scalar) const{ return vektor(x / scalar, y / scalar); }
+    double x, y;
 
-    //vektor operator+(const vektor& v) const { return vektor(x + v.x, y + v.y); }
-    //vektor operator-(const vektor& v) const { return vektor(x - v.x, y - v.y); }
-    //vektor operator*(double scalar) const { return vektor(x * scalar, y * scalar); }
-    //vektor operator/(double scalar) const { return vektor(x / scalar, y / scalar); }
-    double buyukluk() const { return sqrt(x * x + y * y); }
+    vector2D(double x = 0, double y = 0) : x(x), y(y) {}
 
-};
+    vector2D operator+(const vector2D& v) const {
+        return vector2D(x + v.x, y + v.y);
+    }
+    vector2D operator-(const vector2D& v) const {
+        return vector2D(x - v.x, y - v.y);
+    }
+    vector2D operator*(double scalar) const {
+        return vector2D(x * scalar, y * scalar);
+    }
+    vector2D operator/(double scalar) const {
+        return scalar != 0 ? vector2D(x / scalar, y / scalar) : vector2D();
+    }
 
-class cisimler {
-public:
-    double kutle;
-    vektor konum, hiz;
-
-    cisimler(double kutle, vektor konum, vektor hiz) : kutle(kutle), konum(konum), hiz(hiz) {}
-    vektor ivme;
-
-    void guncelle(vektor kuvvet) {
-        ivme = kuvvet.bol(kutle);
-        //ivme = kuvvet / kutle;
-        hiz = hiz.topla(ivme);
-        //hiz = hiz + ivme;
-        konum = konum.topla(hiz);
-        //konum = konum + hiz;
+    vector2D normalize() const {
+        double mag = sqrt(x * x + y * y);
+        if (mag == 0) return vector2D(0, 0);
+        return *this / mag;
     }
 };
 
-class rocket : public cisimler {
+class object {
 public:
-    double puskurtmeHizi, puskurtmeOrani,yakitMiktari;
-    rocket(double m, vektor xy, vektor hiz, double puskurtmeHizi, double puskurtmeOrani, double yakitMiktari);
+    double mass;
+    vector2D position, velocity, acceleration;
 
-    void guncelle(vektor kuvvet);
-};
-rocket::rocket(double m, vektor xy, vektor hiz, double puskurtmeHizi, double puskurtmeOrani, double yakitMiktari) : cisimler(m, xy, hiz), puskurtmeHizi(puskurtmeHizi), puskurtmeOrani(puskurtmeOrani), yakitMiktari(yakitMiktari) {}
-void rocket::guncelle(vektor kuvvet) {}
+    object(double mass_, vector2D position_, vector2D velocity_) : mass(mass_), position(position_), velocity(velocity_) {}
 
-class  node{
-public:
-    cisimler* cisim;
-    node* sonraki;
-    node(cisimler* c) : cisim(c), sonraki(nullptr) {}
-};
+    virtual ~object() {}
 
-class liste {
-public:
-    node* bas;
-    liste() : bas(nullptr) {}
-    void ekle(cisimler* c);
-};
-void liste::ekle(cisimler* c) {}
-
-class karsilastirma {
-public:
-    liste cisimler;
-    void cisimEkle();
-    void ekle();
-    void calistir();
-    void konumHesapla();
-
+    virtual void update(vector2D force) {
+        acceleration = force / mass;
+        velocity = velocity + acceleration * timestep;
+        position = position + velocity * timestep;
+    }
 };
 
-void karsilastirma::calistir() {}
+class rocket : public object {
+public:
+    double exhaustVelocity, exhaustRate, fuelAmount;
+    double massChange;
+
+    rocket(double rocketMass, double fuel, double exVelocity, double rate, vector2D pos, vector2D vel)
+        : object(rocketMass + fuel, pos, vel),
+          exhaustVelocity(exVelocity),
+          exhaustRate(rate),
+          fuelAmount(fuel) {}
+
+    void update(vector2D force) override {
+        if (fuelAmount > 0) {
+            massChange = exhaustRate * timestep;
+            if (massChange > fuelAmount) {
+                massChange = fuelAmount;
+            }
+            fuelAmount -= massChange;
+            mass = std::max(mass - massChange, 0.0);
+            vector2D thrust(0, exhaustVelocity * massChange / timestep);
+            force = force + thrust;
+        }
+        object::update(force);
+    }
+};
+
+class node {
+public:
+    object* obj;
+    node* next;
+    node(object* o) : obj(o), next(nullptr) {}
+};
+
+class objectList {
+public:
+    node* head;
+    objectList() : head(nullptr) {}
+
+    void add(object* o) {
+        node* newNode = new node(o);
+        newNode->next = head;
+        head = newNode;
+    }
+
+    ~objectList() {
+        while (head) {
+            node* temp = head;
+            head = head->next;
+            delete temp->obj;
+            delete temp;
+        }
+    }
+};
+
+class simulation {
+public:
+    objectList objects;
+
+    void addObject(object* o) {
+        objects.add(o);
+    }
+
+    void update() {
+        node* i = objects.head;
+        while (i) {
+            vector2D netForce(0, 0);
+            node* j = objects.head;
+            while (j) {
+                if (i != j) {
+                    vector2D diff = j->obj->position - i->obj->position;
+                    double distance = sqrt(diff.x * diff.x + diff.y * diff.y);
+                    if (distance > 0) {
+                        double forceMagnitude = G * i->obj->mass * j->obj->mass / (distance * distance);
+                        netForce = netForce + diff.normalize() * forceMagnitude;
+                    }
+                }
+                j = j->next;
+            }
+            i->obj->update(netForce);
+            i = i->next;
+        }
+    }
+
+    double totalEnergy() {
+        double energy = 0;
+        node* i = objects.head;
+        while (i) {
+            energy += 0.5 * i->obj->mass * (i->obj->velocity.x * i->obj->velocity.x + i->obj->velocity.y * i->obj->velocity.y);
+            node* j = i->next;
+            while (j) {
+                vector2D diff = j->obj->position - i->obj->position;
+                double distance = sqrt(diff.x * diff.x + diff.y * diff.y);
+                if (distance > 0) {
+                    energy -= (G * i->obj->mass * j->obj->mass) / distance;
+                }
+                j = j->next;
+            }
+            i = i->next;
+        }
+        return energy;
+    }
+
+    vector2D getPosition(int index) {
+        node* temp = objects.head;
+        int count = 1;
+        while (temp) {
+            if (count == index) {
+                return temp->obj->position;
+            }
+            temp = temp->next;
+            count++;
+        }
+        return vector2D();
+    }
+};
